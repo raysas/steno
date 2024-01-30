644769e5cb0f3d850ae097d0dff5927be4355b17
Rayane Adam <91915465+raysas@users.noreply.github.com>
2024-01-30 11:33:13 +0200

    Update create_fasta.py

---
+++ create_fasta.py
@@ -0,0 +1,21 @@
+#!/bin/env/python
+
+#takes a fasta string and creates a fasta file using biopython
+
+import Bio.SeqIO
+
+def create_fasta(fasta_string:str, fasta_name:str)->file:
+    fasta_file = open(fasta_name, "w")
+    
+    #use biopython to write the fasta file
+    Bio.SeqIO.write(fasta_string, fasta_file, "fasta")
+
+    fasta_file.close()
+    return fasta_file
+
+def main():
+    #convert a file into a string
+    fasta_string = open("CP077679.fasta", "r").read()
+    create_fasta(fasta_string, "ref.fasta")
+
+main()
